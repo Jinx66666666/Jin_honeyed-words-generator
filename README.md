5a5ada40373d7e6725e0a0a619a4e366a9a06ffe
黄晶 <huangjing@163.com>
2020-05-20 14:29:36 +0800

    520甜言蜜语

---
+++ README.md
@@ -0,0 +1,36 @@
+<div align="center">
+  <img src="public/android-chrome-192x192.png" alt="logo">
+  <h1>甜(Tu)言(Wei)蜜(Qing)语(Hua)</h1>
+</div>
+
+土味情话，在线生成：[https://github.com/Jinx66666666/Jin_honeyed-words-generator.git]
+
+## 应用截图
+
+### 首页
+
+![demo home](demo.home.png)
+
+### 加载页
+
+![demo load random words](demo.loading.png)
+
+### 结果页
+
+![demo words result](demo.result.png)
+
+### 生成并下载的图片
+
+![demo download](demo.download.png)
+
+### 生成适合分享出去的二维码
+
+
+## 贡献土味情话
+
+所有情话均存储在`src/assets/words.json`文件内，请 clone 代码然后提 PR。
+
+P.S. `|`代表换行
+
+Let's rock it~
+
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { lazy, Suspense, useState } from 'react';
+import styled from 'styled-components';
+
+import { getQueryValue } from './utils';
+import Loading from './components/Loading';
+import ShareQR from './components/ShareQR';
+import LoadingWords from './components/LoadingWords';
+import StartButton from './components/StartButton';
+import RefreshButton from './components/RefreshButton';
+import SaveButton from './components/SaveButton';
+import AddWords from './components/AddWords';
+import Card from './components/Card';
+import Words from './assets/words';
+
+const Header = lazy(() => import('./components/Header'));
+
+const InfoModal = lazy(() => import('./components/InfoModal'));
+const MetooButton = styled(StartButton)`
+  z-index: 998;
+  position: fixed;
+  bottom: 0.6rem;
+  left: 50%;
+  width: 14rem;
+  margin-left: -7rem;
+  animation-delay: ${({ wordCount }) => `${wordCount * 0.3}s`};
+`;
+const wordsIdx = getQueryValue('idx');
+const wordCount = wordsIdx !== '' ? Words[wordsIdx].length : 0;
+const hasWords = wordsIdx !== '';
+const App = () => {
+  const [start, setStart] = useState(hasWords);
+  const [loading, setLoading] = useState(!hasWords);
+  const handleStart = () => {
+    setStart(true);
+    setLoading(true);
+  };
+  const handleDone = () => {
+    setLoading(false);
+  };
+  const handleUpdate = () => {
+    setLoading(true);
+  };
+  return (
+    <Suspense fallback={<Loading />}>
+      {!hasWords && <InfoModal />}
+      {start && !loading && !hasWords && <ShareQR />}
+      {!loading && !hasWords && <AddWords />}
+      <RefreshButton visible={start && !loading && !hasWords} handleUpdate={handleUpdate} />
+      <SaveButton visible={start && !loading && !hasWords} />
+      {!start && <Header handleStart={handleStart} />}
+      <LoadingWords visible={start && loading} handleDone={handleDone} />
+      <Card wordsIdx={wordsIdx} visible={start && !loading} />
+      {start && !loading && hasWords && (
+        <MetooButton
+          wordCount={wordCount}
+          onClick={() => {
+            location.href = location.href.split('?')[0];
+          }}
+        >
+          我也要生成
+        </MetooButton>
+      )}
+    </Suspense>
+  );
+};
+export default App;
